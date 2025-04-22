f0d677bfe0844cb6179cebd7d3683e175755ef63
xt9779 <xt9779@rimac.com.pe>
2025-04-21 19:54:32 -0500

    add new change in the last commit

---
+++ .angular/cache/19.2.6/app/vite/deps/ios.transition-TJL4EYOO.js
@@ -0,0 +1,14 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-56MECWIG.js";
+import "./chunk-IP55N444.js";
+import "./chunk-VYK365E2.js";
+import "./chunk-K4WBEI64.js";
+import "./chunk-ACUVEYEP.js";
+import "./chunk-T653PBGC.js";
+import "./chunk-QHQP2P2Z.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
