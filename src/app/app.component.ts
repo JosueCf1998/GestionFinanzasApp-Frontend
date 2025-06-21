87991a66962fc394e70b0eda32c937da016c76cd
xt9779 <xt9779@rimac.com.pe>
2025-06-21 00:52:42 -0500

    update menu styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,73 @@
-
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
+import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink, IonHeader, IonTitle, IonToolbar, IonAvatar } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import { CommonModule } from '@angular/common'; // Importa el CommonModule
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
-  imports: [RouterLink, RouterLinkActive, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet],
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
+  imports: [
+    IonAvatar,
+    IonToolbar,
+    IonTitle,
+    IonHeader,
+    RouterLink,
+    RouterLinkActive,
+    IonApp,
+    IonSplitPane,
+    IonMenu,
+    IonContent,
+    IonList,
+    IonListHeader,
+    IonNote,
+    IonMenuToggle,
+    IonItem,
+    IonIcon,
+    IonLabel,
+    IonRouterLink,
+    IonRouterOutlet,
+    CommonModule
+  ],
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inicio', url: '/main/home', icon: 'mail' },
-    { title: 'Gráficos', url: '/main/graphics', icon: 'paper-plane' },
-    { title: 'Categorias', url: '/main/categories', icon: 'paper-plane' },
-    { title: 'Cuentas', url: '/main/accounts', icon: 'paper-plane' }
+    { title: "Inicio", url: "/main/home", icon: "home" },
+    { title: "Cuentas", url: "/main/accounts", icon: "money-bag" },
+    { title: "Gráficos", url: "/main/graphics", icon: "chart" },
+    { title: "Categorias", url: "/main/categories", icon: "category" },
   ];
+  public dataUser = {
+    imagen: "https://gravatar.com/avatar/placeholder?s=200&d=mp",
+    nombre: "Juan Pérez",
+    correo: "juan.perez@gmail.com",
+  };
+  usuario: any;
+  usuarioLogueado: boolean = false;
+
+  ngOnInit(): void {
+    this.usuarioLogueado = true;
+    this.usuario = this.dataUser;
+  }
+
   constructor() {
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({
+      mailOutline,
+      mailSharp,
+      paperPlaneOutline,
+      paperPlaneSharp,
+      heartOutline,
+      heartSharp,
+      archiveOutline,
+      archiveSharp,
+      trashOutline,
+      trashSharp,
+      warningOutline,
+      warningSharp,
+      bookmarkOutline,
+      bookmarkSharp,
+    });
   }
 }
